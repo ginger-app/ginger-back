e26a9530957580a263bcf8eded935d3cff03d4c0
roquefore <roquefore@gmail.com>
2020-06-07 14:24:59 +0300

    [SHIT] shit logs

--- src/_modules/appInternal/app.controller.ts
+++ src/_modules/appInternal/app.controller.ts
@@ -1,37 +1,43 @@
 // Core
 import { Controller, Get, Post, Body, Res, Param } from '@nestjs/common';
 
-// Instrumnets
-import * as fs from 'fs';
-import { join } from 'path';
+// Services
+import { RedisStorageService } from '../../_services';
 
 @Controller('app')
 export class AppController {
+  constructor(private readonly redisService: RedisStorageService) {}
+
   @Get('/g-maps')
   getGoogleMapsApiKey() {
     return { success: true, data: process.env.GOOGLE_MAPS_API_KEY };
   }
 
   @Post('/logs')
-  sendLogs(@Body() body, @Param('name') name, @Res() res) {
-    let filepath = '';
-    if (name) {
-      filepath = join('logs', `${name}.txt`);
-      fs.appendFile(filepath, body.logs, err =>
-        err ? err : res.send({ success: true, filepath }),
-      );
-    } else {
-      filepath = join('logs', `latest.txt`);
-      fs.writeFile(filepath, body.logs, err =>
-        err ? err : res.send({ success: true, filepath }),
-      );
+  async sendLogs(@Body() body, @Param('name') name, @Res() res) {
+    try {
+      if (name) {
+        const existingLogs = await this.redisService.get(`logs_${name}`);
+
+        await this.redisService.set(
+          `log_name`,
+          `${existingLogs || ''}\n${body.logs}`,
+        );
+        return res.send({ success: true });
+      } else {
+        await this.redisService.set(`logs_latest`, body.logs);
+        return res.send({ success: true });
+      }
+    } catch (err) {
+      throw err;
     }
   }
 
   @Get('/logs')
-  getLogs(@Res() res, @Param('name') name) {
-    const filepath = join('logs', `${name || 'latest'}.txt`);
-    const data = fs.readFileSync(filepath);
+  async getLogs(@Res() res, @Param('name') name) {
+    const data = await this.redisService.get(
+      name ? `logs_${name}` : 'logs_latest',
+    );
 
     res.send(data);
   }
--- src/_modules/appInternal/app.module.ts
+++ src/_modules/appInternal/app.module.ts
@@ -4,7 +4,11 @@ import { Module } from '@nestjs/common';
 // Controllers
 import { AppController } from './app.controller';
 
+// Providers
+import { RedisStorageService } from '../../_services';
+
 @Module({
   controllers: [AppController],
+  providers: [RedisStorageService],
 })
 export class AppInternalModule {}
